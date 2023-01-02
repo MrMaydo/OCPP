86ca50add105f66f98c27fc8b1330d7448e55702
MrMaydo <89995856+MrMaydo@users.noreply.github.com>
2023-01-02 21:14:42 +0100

    Create fromJsonObject() method

--- src/main/java/maydo/ocpp/utils/JsonTools.java
+++ src/main/java/maydo/ocpp/utils/JsonTools.java
@@ -1,12 +1,12 @@
 package maydo.ocpp.utils;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import maydo.ocpp.msgDef.annotations.Optional;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -36,7 +36,7 @@ public class JsonTools {
         String fieldName = field.getName();
 
         if (field.get(object) == null) {
-            if(field.isAnnotationPresent(Optional.class))
+            if (field.isAnnotationPresent(Optional.class))
                 return;
 
             jsonObject.addProperty(fieldName, (String) null);
@@ -56,7 +56,7 @@ public class JsonTools {
             jsonObject.addProperty(fieldName, dateFormat.format(fieldValue));
         } else if (fieldType.equals("List")) {
             JsonArray jsonArray = new JsonArray();
-            copyListToJsonArray(jsonArray, (List) fieldValue);
+            copyListToJsonArray(jsonArray, (List<?>) fieldValue);
             jsonObject.add(fieldName, jsonArray);
         } else if (field.getType().getSuperclass().getSimpleName().equals("Enum")) {
             jsonObject.addProperty(fieldName, fieldValue.toString());
@@ -91,4 +91,163 @@ public class JsonTools {
             }
         }
     }
+
+
+    public static <T> void fromJsonString(T object, String jsonString) {
+        JsonObject jsonObject = JsonParser.parseString(jsonString).getAsJsonObject();
+
+        fromJsonObject(object, jsonObject);
+    }
+
+    public static <T> void fromJsonObject(T object, JsonObject jsonObject) {
+        Class classType = object.getClass();
+        Field[] declaredFields = classType.getDeclaredFields();
+        try {
+            for (Field field : declaredFields) {
+                setFieldFromJson(object, jsonObject, field);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getCause().toString());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static <T> void setFieldFromJson(T object, JsonObject jsonObject, Field field) throws Exception {
+        String fieldType;
+        String fieldName;
+        field.setAccessible(true);
+
+        Class<?> clazz = Class.forName(field.getType().getTypeName());
+//        Constructor<?> constructor = clazz.getConstructor();
+
+//        field.set(object, constructor.newInstance());
+        fieldType = field.getType().getSimpleName();
+        fieldName = field.getName();
+//        String parameter = jsonObject.get(fieldName).toString().replaceAll("\"", "");
+        JsonElement parameter = jsonObject.get(fieldName);
+        System.out.println("parameter:" + parameter);
+        if(parameter.isJsonNull()){
+            field.set(object, null);
+            return;
+        }
+        if (fieldType.equals("Integer")) {
+            field.set(object, parameter.getAsInt());
+        } else if (fieldType.equals("Float")) {
+            field.set(object, parameter.getAsFloat());
+        } else if (fieldType.equals("Boolean")) {
+            field.set(object, parameter.getAsBoolean());
+        } else if (fieldType.equals("String")) {
+            field.set(object, parameter.getAsString());
+        } else if (fieldType.equals("Date")) {
+            Date date = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+            date.setTime(dateFormat.parse(parameter.getAsString()).getTime());
+            field.set(object, date);
+        } else if (fieldType.equals("List")) {
+            List<?> list;
+            JsonArray jsonArray = parameter.getAsJsonArray();
+            int start = field.getGenericType().toString().lastIndexOf("<") + 1;
+            int stop = field.getGenericType().toString().lastIndexOf(">");
+            String strListType = field.getGenericType().getTypeName().substring(start, stop);
+            Class<?> listType = Class.forName(strListType);
+            list = copyJsonArrayToList(jsonArray, listType);
+            field.set(object, list);
+        } else if (field.getType().getSuperclass().getSimpleName().equals("Enum")) {
+//            String parameter = jsonObject.get(fieldName).toString().replaceAll("\"", "");
+            Method method = field.getType().getDeclaredMethod("fromValue", String.class);
+            field.set(object, method.invoke(null, parameter.getAsString()));
+        } else {
+            Constructor<?> constructor = clazz.getConstructor();
+            field.set(object, constructor.newInstance());
+            fromJsonObject(field.get(object), (JsonObject) parameter);
+//            Method method = field.getType().getDeclaredMethod("fromJsonString", String.class);
+//            field.set(object, method.invoke(null, parameter.getAsString()));
+        }
+    }
+
+    private static <T> List<T> copyJsonArrayToList(JsonArray jsonArray, Class<?> listType) throws Exception {
+
+        if (jsonArray == null) {
+            return null;
+        }
+
+        if (listType.getSimpleName().equals("Integer")) {
+            return (List<T>) addIntToList(jsonArray);
+        } else if (listType.getSimpleName().equals("Float")) {
+            return (List<T>) addFloatToList(jsonArray);
+        } else if (listType.getSimpleName().equals("Boolean")) {
+            return (List<T>) addBoolToList(jsonArray);
+        } else if (listType.getSimpleName().equals("String")) {
+            return (List<T>) addStringToList(jsonArray);
+        } else if (listType.getSimpleName().equals("Date")) {
+            return (List<T>) addDateToList(jsonArray);
+        } else if (listType.getSuperclass().getSimpleName().equals("Enum")) {
+            return (List<T>) addEnumToList(jsonArray, listType);
+        } else {
+//            jsonArray.add(toJsonObject(item));
+            return null;
+        }
+    }
+
+    private static Object addDateToList(JsonArray jsonArray) throws ParseException {
+        List<Date> list = new ArrayList<>();
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        Date date;
+
+        for (JsonElement element : jsonArray) {
+            date = new Date();
+            date.setTime(dateFormat.parse(element.getAsString()).getTime());
+            list.add(date);
+        }
+        return list;
+    }
+
+    private static Object addEnumToList(JsonArray jsonArray, Class<?> listType) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        List<Enum<? extends Enum<?>>> list = new ArrayList<>();
+        Method method = listType.getDeclaredMethod("fromValue", String.class);
+
+        for (JsonElement element : jsonArray) {
+            list.add((Enum<? extends Enum<?>>) method.invoke(null, element.getAsString()));
+        }
+        return list;
+    }
+
+    private static Object addStringToList(JsonArray jsonArray) {
+        List<String> list = new ArrayList<>();
+        for (JsonElement element : jsonArray) {
+            list.add(element.toString().replace("\"", ""));
+        }
+        return list;
+    }
+
+    private static Object addBoolToList(JsonArray jsonArray) {
+        List<Boolean> list = new ArrayList<>();
+        for (JsonElement element : jsonArray) {
+            list.add(element.getAsBoolean());
+        }
+        return list;
+    }
+
+    private static List<Integer> addIntToList(JsonArray jsonArray) {
+        List<Integer> list = new ArrayList<>();
+        for (JsonElement element : jsonArray) {
+            list.add(element.getAsInt());
+        }
+        return list;
+    }
+
+    private static List<Float> addFloatToList(JsonArray jsonArray) {
+        List<Float> list = new ArrayList<>();
+        for (JsonElement element : jsonArray) {
+            list.add(element.getAsFloat());
+        }
+        return list;
+    }
+
+//    private static <T> T getInstance(T object, String className) throws Exception {
+//        Class<?> clazz = Class.forName(className);
+//        Constructor<?> constructor = clazz.getConstructor(String.class);
+//        constructor.newInstance(new Object());
+//
+//    }
 }
--- src/test/java/maydo/ocpp/utils/JsonToolsTest.java
+++ src/test/java/maydo/ocpp/utils/JsonToolsTest.java
@@ -2,6 +2,7 @@ package maydo.ocpp.utils;
 
 import com.google.gson.JsonObject;
 import maydo.ocpp.utils.testObjects.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,11 +12,12 @@ import java.util.Date;
 import java.util.List;
 
 import static maydo.ocpp.utils.JsonTools.toJsonObject;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class JsonToolsTest {
     JsonObject jsonObject;
     String expectedJsonString;
+    String jsonString;
 
 
     @BeforeEach
@@ -26,7 +28,6 @@ class JsonToolsTest {
     @Test
     void toJsonObject_Integer() {
         IntObject object = new IntObject();
-
         expectedJsonString = "{\"intValue\":5}";
         object.setIntValue(5);
         assertEquals(expectedJsonString, toJsonObject(object).toString(),
@@ -312,8 +313,7 @@ class JsonToolsTest {
                 "\"listObject\":{\"listValue\":[\"a\",\"b\",\"c\"]}}," +
 
                 "{\"intObject\":null," +
-                "\"strObject\":null," +
-                "\"listObject\":null" +
+                "\"strObject\":null" +
                 "}" +
                 "]}";
 
@@ -333,4 +333,316 @@ class JsonToolsTest {
 
         return cal.getTime();
     }
+
+
+    @Test
+    void fromJsonString_Integer() {
+        jsonString = "{\"intValue\":5}";
+        IntObject object = new IntObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(5, object.getIntValue(),
+                "Failed at converting Json String to Integer object");
+    }
+
+    @Test
+    void fromJsonString_NullInteger() {
+        jsonString = "{\"intValue\":null}";
+        IntObject object = new IntObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertNull(object.getIntValue(),
+                "Failed at converting Json String to Integer object");
+    }
+
+    @Test
+    void fromJsonString_Float() {
+        jsonString = "{\"floatValue\":2.7}";
+        FloatObject object = new FloatObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals( (float) 2.7, object.getFloatValue(),
+                "Failed at converting Json String to Float object");
+    }
+
+    @Test
+    void fromJsonString_NullFloat() {
+        jsonString = "{\"floatValue\":null}";
+        FloatObject object = new FloatObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertNull(object.getFloatValue(),
+                "Failed at converting Json String to Float object");
+    }
+
+    @Test
+    void fromJsonString_String() {
+        jsonString = "{\"strValue\":\"abc\"}";
+        StrObject object = new StrObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals("abc", object.getStrValue(),
+                "Failed at converting Json String to String object");
+    }
+
+    @Test
+    void fromJsonString_NullString() {
+        jsonString = "{\"strValue\":null}";
+        StrObject object = new StrObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertNull(object.getStrValue(),
+                "Failed at converting Json String to String object");
+    }
+
+    @Test
+    void fromJsonString_Boolean() {
+        jsonString = "{\"boolValue\":true}";
+        BoolObject object = new BoolObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(true, object.getBoolValue(),
+                "Failed at converting Json String to Boolean object");
+    }
+
+    @Test
+    void fromJsonString_NullBoolean() {
+        jsonString = "{\"boolValue\":null}";
+        BoolObject object = new BoolObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertNull(object.getBoolValue(),
+                "Failed at converting Json String to Boolean object");
+    }
+
+    @Test
+    void fromJsonString_Enum() {
+        jsonString = "{\"enumValue\":\"boolean\"}";
+        EnumObject object = new EnumObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(TestEnum.BOOLEAN, object.getEnumValue(),
+                "Failed at converting Json String to Enum object");
+    }
+
+    @Test
+    void fromJsonString_NullEnum() {
+        jsonString = "{\"enumValue\":null}";
+        EnumObject object = new EnumObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertNull(object.getEnumValue(),
+                "Failed at converting Json String to Enum object");
+    }
+
+    @Test
+    void fromJsonString_Date() {
+        jsonString = "{\"dateValue\":\"2022-12-03T21:46:59.123Z\"}";
+        DateObject object = new DateObject();
+        Date expectedDate = createDate(2022, 12, 3, 21, 46, 59, 123);
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedDate, object.getDateValue(),
+                "Failed at converting Json String to Date object");
+    }
+
+    @Test
+    void fromJsonString_NullDate() {
+        jsonString = "{\"dateValue\":null}";
+        DateObject object = new DateObject();
+        JsonTools.fromJsonString(object, jsonString);
+        assertNull(object.getDateValue(),
+                "Failed at converting Json String to Date object");
+    }
+    @Test
+    void fromJsonString_ComplexObject() {
+        jsonString = "{\"intObject\":{\"intValue\":6}," +
+                "\"strObject\":{\"strValue\":\"abc\"}," +
+                "\"dateObject\":{\"dateValue\":null}," +
+                "\"listObject\":null" +
+                "}";
+        ComplexObject expectedObject = new ComplexObject();
+        ComplexObject object = new ComplexObject();
+
+        expectedObject.setIntObject(new IntObject());
+        expectedObject.setStrObject(new StrObject());
+        expectedObject.setDateObject(new DateObject());
+        expectedObject.getIntObject().setIntValue(6);
+        expectedObject.getStrObject().setStrValue("abc");
+
+
+        JsonTools.fromJsonString(object, jsonString);
+        System.out.println(expectedObject.equals(object));
+        assertTrue(expectedObject.equals(object),
+                "Failed at converting Json String to CComplexObject object");
+    }
+
+
+    @Test
+    void fromJsonString_IntList() {
+        jsonString = "{\"listValue\":[1,2,4,6]}";
+        IntList object = new IntList();
+        List<Integer> expectedList = new ArrayList<>();
+        expectedList.add(1);
+        expectedList.add(2);
+        expectedList.add(4);
+        expectedList.add(6);
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<Integer> object");
+    }
+
+
+    @Test
+    void fromJsonString_EmptyIntList() {
+        jsonString = "{\"listValue\":[]}";
+        IntList object = new IntList();
+        List<Integer> expectedList = new ArrayList<>();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<Integer> object");
+    }
+
+    @Test
+    void fromJsonString_FloatList() {
+        jsonString = "{\"listValue\":[1.5,2.3,4.8]}";
+        FloatList object = new FloatList();
+        List<Float> expectedList = new ArrayList<>();
+        expectedList.add(1.5F);
+        expectedList.add(2.3F);
+        expectedList.add(4.8F);
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<Float> object");
+    }
+
+    @Test
+    void fromJsonString_EmptyFloatList() {
+        jsonString = "{\"listValue\":[]}";
+        FloatList object = new FloatList();
+        List<Float> expectedList = new ArrayList<>();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<Float> object");
+    }
+
+    @Test
+    void fromJsonString_StrList() {
+        jsonString = "{\"listValue\":[\"x\",\"y\",\"z\"]}";
+        StrList object = new StrList();
+        List<String> expectedList = new ArrayList<>();
+        expectedList.add("x");
+        expectedList.add("y");
+        expectedList.add("z");
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<String> object");
+    }
+
+    @Test
+    void fromJsonString_EmptyStrList() {
+        jsonString = "{\"listValue\":[]}";
+        StrList object = new StrList();
+        List<String> expectedList = new ArrayList<>();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<String> object");
+    }
+
+    @Test
+    void fromJsonString_BoolList() {
+        jsonString = "{\"listValue\":[true, false, true, true]}";
+        BoolList object = new BoolList();
+        List<Boolean> expectedList = new ArrayList<>();
+        expectedList.add(true);
+        expectedList.add(false);
+        expectedList.add(true);
+        expectedList.add(true);
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<Boolean> object");
+    }
+
+    @Test
+    void fromJsonString_EmptyBoolList() {
+        jsonString = "{\"listValue\":[]}";
+        BoolList object = new BoolList();
+        List<Boolean> expectedList = new ArrayList<>();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<Boolean> object");
+    }
+
+    @Test
+    void fromJsonString_EnumList() {
+        jsonString = "{\"listValue\":[\"string\", \"integer\", \"boolean\"]}";
+        EnumList object = new EnumList();
+        List<TestEnum> expectedList = new ArrayList<>();
+        expectedList.add(TestEnum.STRING);
+        expectedList.add(TestEnum.INTEGER);
+        expectedList.add(TestEnum.BOOLEAN);
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<Enum> object");
+    }
+
+    @Test
+    void fromJsonString_EmptyEnumList() {
+        jsonString = "{\"listValue\":[]}";
+        EnumList object = new EnumList();
+        List<TestEnum> expectedList = new ArrayList<>();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<Enum> object");
+    }
+
+    @Test
+    void fromJsonString_DateList() {
+        jsonString = "{\"listValue\":[" +
+                "\"2022-12-03T21:46:59.123Z\", " +
+                "\"2022-10-05T09:46:59.13Z\", " +
+                "\"2022-01-31T12:46:13.950Z\"" +
+                "]}";
+        DateList object = new DateList();
+        List<Date> expectedList = new ArrayList<>();
+
+        expectedList.add(createDate(2022, 12, 3, 21, 46, 59, 123));
+        expectedList.add(createDate(2022, 10, 5, 9, 46, 59, 13));
+        expectedList.add(createDate(2022, 1, 31, 12, 46, 13, 950));
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<Date> object");
+    }
+
+    @Test
+    void fromJsonString_EmptyDateList() {
+        jsonString = "{\"listValue\":[]}";
+        DateList object = new DateList();
+        List<Date> expectedList = new ArrayList<>();
+        JsonTools.fromJsonString(object, jsonString);
+        assertEquals(expectedList, object.getListValue(),
+                "Failed at converting Json String to List<Date> object");
+    }
+
+//    @Test
+//    void fromJsonString_ComplexList() {
+//        jsonString = "{\"listValue\":[" +
+//                "{\"intObject\":6,\"strObject\":\"abc\", \"dateObject\":null}," +
+//                "{\"intObject\":3,\"strObject\":\"cde\"}," +
+//                "{}" +
+//                "]}";
+//        ComplexList object = new ComplexList();
+//        List<ComplexObject> expectedList = new ArrayList<>();
+//        ComplexObject item1 = new ComplexObject();
+//        ComplexObject item2 = new ComplexObject();
+//        ComplexObject item3 = new ComplexObject();
+//
+//        item1.setIntObject(new IntObject());
+//        item1.setStrObject(new StrObject());
+//        item1.setDateObject(new DateObject());
+//        item1.getIntObject().setIntValue(6);
+//        item1.getStrObject().setStrValue("abc");
+//
+//        item2.setIntObject(new IntObject());
+//        item2.setStrObject(new StrObject());
+//        item2.getIntObject().setIntValue(3);
+//        item2.getStrObject().setStrValue("cde");
+//
+//        expectedList.add(item1);
+//        expectedList.add(item2);
+//        expectedList.add(item3);
+//
+//        JsonTools.fromJsonString(object, jsonString);
+//        assertEquals(expectedList, object.getListValue(),
+//                "Failed at converting Json String to List<Complex> object");
+//    }
 }
